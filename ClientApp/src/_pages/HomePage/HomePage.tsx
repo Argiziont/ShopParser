8e200ec18f80c28f236f17909f78d452ff43f7e0
Argiziont <tools33333@gmail.com>
2021-03-13 13:51:56 +0200

    Parallel parsing

--- ClientApp/src/_pages/HomePage/HomePage.tsx
+++ ClientApp/src/_pages/HomePage/HomePage.tsx
@@ -1,5 +1,46 @@
+import { Button, Grid, makeStyles, TextField } from "@material-ui/core";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 import React from "react";
 
+const useStyles = makeStyles((theme) => ({
+  rootBox: {
+    marginTop: theme.spacing(5),
+  },
+  urlField: {
+    margin: theme.spacing(5),
+  },
+}));
 export const HomePage: React.FC = () => {
-  return <React.Fragment>{"Hello world"}</React.Fragment>;
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justify="center"
+        className={classes.rootBox}
+      >
+        <Grid container xs={4}></Grid>
+
+        <Grid
+          container
+          xs={4}
+          spacing={2}
+          justify="center"
+          alignItems="flex-end"
+        >
+          <Grid item>
+            <TextField label="Site URL" variant="standard" />
+          </Grid>
+          <Grid item>
+            <Button variant="contained" endIcon={<CloudUploadIcon />}>
+              {"Submit"}
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid container xs={4}></Grid>
+      </Grid>
+    </React.Fragment>
+  );
 };
--- PrjModule25_Parser/Controllers/WebScraperController.cs
+++ PrjModule25_Parser/Controllers/WebScraperController.cs
@@ -1,4 +1,6 @@
-﻿using AngleSharp;
+﻿using System;
+using System.Collections.Concurrent;
+using AngleSharp;
 using AngleSharp.Dom;
 using AngleSharp.Html.Dom;
 using Microsoft.AspNetCore.Mvc;
@@ -26,74 +28,88 @@ namespace PrjModule25_Parser.Controllers
                 .Select(m => ((IHtmlAnchorElement)m).Href)
                 .ToList();
 
-            var elements = new List<CarAdvert>();
-            foreach (var link in linksToProducts)
+            var elements = ParallelParsing(context, linksToProducts);
+            
+            return Ok(elements);
+
+        }
+        private static async Task<CarAdvert> FindDataInsideProductPageAsync(IBrowsingContext context,string subUrl)
+        {
+            var product = await context.OpenAsync(subUrl);
+
+            var title = product.QuerySelector("*[data-qaid='product_name']")?.InnerHtml ?? "";
+
+            var sku = product.QuerySelector("span[data-qaid='product-sku']")?.InnerHtml ?? "";
+
+            var presence = product.QuerySelector("span[data-qaid='product_presence']")?.FirstElementChild?.InnerHtml ?? "";
+
+            var descriptionChildren = product.QuerySelector("div[data-qaid='descriptions']")?.Children;
+            var description = UnScrubDiv(descriptionChildren).Replace(@"&nbsp;", "");
+
+            var priceSelector = (IHtmlSpanElement)product.QuerySelector("span[data-qaid='product_price']");
+            var fullPriceSelector = (IHtmlSpanElement)product.QuerySelector("span[data-qaid='price_without_discount']");
+            var optPriceSelector = (IHtmlSpanElement)product.QuerySelector("span[data-qaid='opt_price']");
+
+
+            var price = priceSelector?.Dataset["qaprice"] ?? "";
+            var currency = priceSelector?.Dataset["qacurrency"] ?? "";
+
+            var fullPrice = fullPriceSelector?.Dataset["qaprice"] ?? "";
+            var fullCurrency = fullPriceSelector?.Dataset["qacurrency"] ?? "";
+
+            var optPrice = optPriceSelector?.Dataset["qaprice"] ?? "";
+            var optCurrency = optPriceSelector?.Dataset["qacurrency"] ?? "";
+
+
+            //Picking image list
+            var imageSrcList = product.QuerySelector("div[data-qaid='image_block']")//<Upper div>
+                ?.Children//<Lower divs>
+                ?.First(m => m.ClassList
+                    .Contains("ek-grid__item_width_expand") == false) //<Lower div with thumbnails>
+                ?.Children//<Ul>
+                ?.First()
+                ?.Children//<Li>
+                ?.Select(i => ((IHtmlImageElement)i//<Img>
+                    .QuerySelector("img[data-qaid='image_thumb']"))?.Source)//Src="Urls"
+                .ToList();
+
+            return new CarAdvert()
+            {
+                Currency = currency,
+                Price = price,
+                FullCurrency = fullCurrency,
+                FullPrice = fullPrice,
+                OptCurrency = optCurrency,
+                OptPrice = optPrice,
+                Description = description,
+                Presence = presence,
+                ScuCode = sku,
+                Title = title,
+                ImageUrls = imageSrcList
+            };
+        }
+
+        private static IList<CarAdvert> ParallelParsing(IBrowsingContext context, IEnumerable<string> linksToProducts)
+        {
+            var parallelElements = new ConcurrentBag<CarAdvert>();
+
+            Parallel.ForEach(linksToProducts, link =>
             {
                 try
                 {
-                    var product = await context.OpenAsync(link);
-
-                    var title = product.QuerySelector("*[data-qaid='product_name']")?.InnerHtml ?? "";
-
-                    var sku = product.QuerySelector("span[data-qaid='product-sku']")?.InnerHtml ?? "";
-
-                    var presence = product.QuerySelector("span[data-qaid='product_presence']")?.FirstElementChild?.InnerHtml ?? "";
-
-                    var descriptionChildren = product.QuerySelector("div[data-qaid='descriptions']")?.Children;
-                    var description = UnScrubDiv(descriptionChildren).Replace(@"&nbsp;", "");
-
-                    var priceSelector = (IHtmlSpanElement)product.QuerySelector("span[data-qaid='product_price']");
-                    var fullPriceSelector = (IHtmlSpanElement)product.QuerySelector("span[data-qaid='price_without_discount']");
-                    var optPriceSelector = (IHtmlSpanElement)product.QuerySelector("span[data-qaid='opt_price']");
-
-
-                    var price = priceSelector?.Dataset["qaprice"] ?? "";
-                    var currency = priceSelector?.Dataset["qacurrency"] ?? "";
-
-                    var fullPrice = fullPriceSelector?.Dataset["qaprice"] ?? "";
-                    var fullCurrency = fullPriceSelector?.Dataset["qacurrency"] ?? "";
-
-                    var optPrice = optPriceSelector?.Dataset["qaprice"] ?? "";
-                    var optCurrency = optPriceSelector?.Dataset["qacurrency"] ?? "";
-
-                    
-                    //Picking image list
-                    var imageSrcList = product.QuerySelector("div[data-qaid='image_block']")//<Upper div>
-                        ?.Children//<Lower divs>
-                        ?.First(m => m.ClassList
-                            .Contains("ek-grid__item_width_expand") == false) //<Lower div with thumbnails>
-                        ?.Children//<Ul>
-                        ?.First()
-                        ?.Children//<Li>
-                        ?.Select(i => ((IHtmlImageElement)i//<Img>
-                            .QuerySelector("img[data-qaid='image_thumb']"))?.Source)//Src="Urls"
-                        .ToList();
-                    
-                    elements.Add(new CarAdvert()
-                    {
-                        Currency = currency,
-                        Price = price,
-                        FullCurrency = fullCurrency,
-                        FullPrice = fullPrice,
-                        OptCurrency = optCurrency,
-                        OptPrice = optPrice,
-                        Description = description,
-                        Presence = presence,
-                        ScuCode = sku,
-                        Title = title,
-                        ImageUrls = imageSrcList
-                    });
+                    var productInfo =FindDataInsideProductPageAsync(context, link).Result;
+                    parallelElements.Add(productInfo);
                     
                 }
                 catch
                 {
                     // ignored
                 }
-            }
-            return Ok(elements);
-
+            });
+           
+            return parallelElements.ToList();
         }
-
+        
         private static string UnScrubDiv(IEnumerable<IElement> divElements)
         {
             var unScrubText = "";
