c9aa44b397934d9b445fbfbc53e82cf065884139
Argiziont <tools33333@gmail.com>
2021-05-06 15:48:12 +0300

    ProductController tests coverage is 100%

--- ShopParserApi.Tests/ProductControllerTest.cs
+++ ShopParserApi.Tests/ProductControllerTest.cs
@@ -9,9 +9,11 @@ using Moq;
 using Newtonsoft.Json;
 using ShopParserApi.Controllers;
 using ShopParserApi.Models;
+using ShopParserApi.Models.Helpers;
 using ShopParserApi.Models.Hubs;
 using ShopParserApi.Models.Hubs.Clients;
 using ShopParserApi.Models.Json_DTO;
+using ShopParserApi.Models.ResponseModels;
 using ShopParserApi.Services;
 using ShopParserApi.Services.Interfaces;
 using Xunit;
@@ -41,7 +43,7 @@ namespace ShopParserApi.Tests
             var productJson2 = new ProductJson { Title = "Two" };
             var productJson3 = new ProductJson { Title = "Three" };
 
-            var product1 = new ProductData { Title = "One", JsonData = JsonConvert.SerializeObject(productJson1),Url = "Url1",ExternalId = "1"};
+            var product1 = new ProductData { Title = "One", JsonData = JsonConvert.SerializeObject(productJson1),Url = "Url1",ExternalId = "1",ProductState = ProductState.Success};
             var product2 = new ProductData { Title = "Two", JsonData = JsonConvert.SerializeObject(productJson2), Url = "Url2", ExternalId = "2" };
             var product3 = new ProductData { Title = "Three", JsonData = JsonConvert.SerializeObject(productJson3), Url = "Url3", ExternalId = "3" };
 
@@ -136,16 +138,16 @@ namespace ShopParserApi.Tests
 
             var productServiceMock = new Mock<IProductService>();
 
-            productServiceMock.Setup(service => service.InsertProductPageIntoDb("Url1")).ReturnsAsync(() => context.Products.FirstOrDefault(p => p.Url == "Url1"));
+            productServiceMock.Setup(service => service.InsertProductPageIntoDb("Url2")).ReturnsAsync(() => context.Products.FirstOrDefault(p => p.Url == "Url2"));
 
             mockHubContext.Setup(service => service.Clients).Returns(mockClients.Object);
             mockClients.Setup(service => service.All).Returns(mockClient.Object);
-            mockClient.Setup(service => service.ReceiveMessage($"Product with name id: 1 was updated successfully"));
+            mockClient.Setup(service => service.ReceiveMessage("Product with name id: 2 was updated successfully"));
 
             var controller = new ProductController(context, mockHubContext.Object, productServiceMock.Object);
 
             //Act
-            var result = await controller.ParseSingleProductInsideCompanyPageAsync("1");
+            var result = await controller.ParseSingleProductInsideCompanyPageAsync("2");
             var okResult = result as OkObjectResult;
 
             //Assert
@@ -155,8 +157,8 @@ namespace ShopParserApi.Tests
             var okResultValue = okResult.Value as ProductData;
             Assert.NotNull(okResultValue);
 
-            Assert.Equal("One", okResultValue.Title);
-            Assert.Equal(1, okResultValue.Id);
+            Assert.Equal("Two", okResultValue.Title);
+            Assert.Equal(2, okResultValue.Id);
         }
         #endregion
 
@@ -182,6 +184,118 @@ namespace ShopParserApi.Tests
             Assert.NotNull(okResultValue);
 
             Assert.Equal("Two", okResultValue.Title);
+        }
+        #endregion
+
+        #region CanGetProductsByCompanyIdAsyncResponseProductList
+        [Fact]
+        public async Task Can_getProductsByCompanyIdAsync_responseProductList()
+        {
+            //Arrange
+            await using var context = new ApplicationDb(ContextOptions);
+            Seed();
+
+            var controller = new ProductController(context, null, null);
+
+            //Act
+            var result = await controller.GetProductsByCompanyIdAsync(1);
+            var okResult = result as OkObjectResult;
+
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+
+            var okResultValue = okResult.Value as IEnumerable<ResponseProduct>;
+            Assert.NotNull(okResultValue);
+
+            var responseProducts = okResultValue as ResponseProduct[] ?? okResultValue.ToArray();
+
+            Assert.Single(responseProducts);
+            Assert.Equal("One",responseProducts.First().Title);
+        }
+        #endregion
+
+        #region CanGetProductsByCategoryIdAsyncResponseProductList
+        [Fact]
+        public async Task Can_getProductsByCategoryIdAsync_responseProductList()
+        {
+            //Arrange
+            await using var context = new ApplicationDb(ContextOptions);
+            Seed();
+
+            var controller = new ProductController(context, null, null);
+
+            //Act
+            var result = await controller.GetProductsByCategoryIdAsync(1);
+            var okResult = result as OkObjectResult;
+
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+
+            var okResultValue = okResult.Value as IEnumerable<ResponseProduct>;
+            Assert.NotNull(okResultValue);
+
+            var responseProducts = okResultValue as ResponseProduct[] ?? okResultValue.ToArray();
+
+            Assert.Single(responseProducts);
+            Assert.Equal("One", responseProducts.First().Title);
+        }
+        #endregion
+
+        #region CanGetPagedProductsByCompanyIdAsyncResponseProductList
+        [Fact]
+        public async Task Can_getPagedProductsByCompanyIdAsync_responseProductList()
+        {
+            //Arrange
+            await using var context = new ApplicationDb(ContextOptions);
+            Seed();
+
+            var controller = new ProductController(context, null, null);
+
+            //Act
+            var result = await controller.GetPagedProductsByCompanyIdAsync(1,0,1);
+            var okResult = result as OkObjectResult;
+
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+
+            var okResultValue = okResult.Value as IEnumerable<ResponseProduct>;
+            Assert.NotNull(okResultValue);
+
+            var responseProducts = okResultValue as ResponseProduct[] ?? okResultValue.ToArray();
+
+            Assert.Single(responseProducts);
+            Assert.Equal("One", responseProducts.First().Title);
+        }
+        #endregion
+
+        #region CanGetPagedProductsByCategoryIdAsyncResponseProductList
+        [Fact]
+        public async Task Can_getPagedProductsByCategoryIdAsync_responseProductList()
+        {
+            //Arrange
+            await using var context = new ApplicationDb(ContextOptions);
+            Seed();
+
+            var controller = new ProductController(context, null, null);
+
+            //Act
+            var result = await controller.GetPagedProductsByCategoryIdAsync(1,0,1);
+            var okResult = result as OkObjectResult;
+
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+
+            var okResultValue = okResult.Value as IEnumerable<ResponseProduct>;
+            Assert.NotNull(okResultValue);
+
+            var responseProducts = okResultValue as ResponseProduct[] ?? okResultValue.ToArray();
+
+            Assert.Single(responseProducts);
+            Assert.Equal("One", responseProducts.First().Title);
         }
         #endregion
 
